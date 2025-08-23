851bcd7065d76c9573bd3566f40bcd1b8d79f191
therayy <raafat@ibm.com>
2025-08-23 12:03:17 -0700

    bash
    Signed-off-by: therayy <raafat@ibm.com>

--- labs-repo/6.3-bash/helm.sh
+++ labs-repo/6.3-bash/helm.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
 export APP_NAME=myapp
-echo "Installing Helm chart for myapp..."
-
+echo "Installing Helm chart for $APP_NAME..."
 cd ..
 cd 6.2-helm/myapp
-
 helm install $APP_NAME . 
-
 echo "Installing Helm chart for $APP_NAME............"
-
 sleep 3
-
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-
 sleep 3
-
 oc status 
-
-curl $(oc get route myapp -o jsonpath='{.spec.host}')
-
+curl $(oc get route $APP_NAME -o jsonpath='{.spec.host}')
 sleep 3 
-
-
 echo "\n Deleting $APP_NAME"
-
 sleep 90
-
 helm uninstall $APP_NAME
+echo "\n $APP_NAME deleted"
